198fd5371e8af350544e3cfaecc34746ef3e7c10
MuhachevaPolina <muhachevapolina@icloud.com>
2024-03-25 19:57:43 +0300

    edited solving

--- inner_run.sh
+++ inner_run.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-# docker pull ubuntu
-docker build -t inner_img home/.
-docker run -it --name inner_container inner_img
+if [ $1 = "f" ]
+then
+    docker build -t inner_img home/.
+    docker run -d --name inner_container inner_img
+fi
+
+if [ $1 = "i" ]
+    then
+    docker run -d --name inner_container $2
+fi
--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-docker run --privileged -d --name outer_container docker:dind # /bin/sh
-docker cp Dockerfile outer_container:/home/Dockerfile
+docker run --privileged -d --name outer_container docker:dind
+
+if [ -n $1 ] 
+then
+    docker cp Dockerfile outer_container:/home/Dockerfile
+fi
+
 docker cp inner_run.sh outer_container:/home/inner_run.sh
-docker exec -ti outer_container sh -c "echo "hi""
-docker exec -ti outer_container sh -c "chmod 777 /home/inner_run.sh"
 docker exec -it outer_container /bin/sh
-# docker exec inner_container echo "hi"
-# docker exec -t outer_container sh -c "sh /home/inner_run.sh"
